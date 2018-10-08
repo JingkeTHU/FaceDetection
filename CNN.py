9c90de15cb3c5510387fa185b59d6e3471df216c
Jingke Zhang <thujingke@163.com>
2018-10-08 16:31:01 +0800

    V1.0 Workable CNN

--- CNN.py
+++ CNN.py
@@ -48,44 +48,68 @@ def convolution_model(float_image_batch):
     float_image_batch = tf.reshape(image_sequence, [-1, 800, 800, 1])
     conv2d_layer_one = tf.contrib.layers.convolution2d(
         float_image_batch,
-        num_outputs=12,  # The number of filters to generate
+        num_outputs=16,  # The number of filters to generate
         kernel_size=(7, 7),  # It's only the filter height and width.
         activation_fn=tf.nn.relu,
         stride=(2, 2))
     print('second')
     pool_layer_one = tf.nn.max_pool(conv2d_layer_one,
-                                    ksize=[1, 2, 2, 1],
-                                    strides=[1, 2, 2, 1],
+                                    ksize=[1, 4, 4, 1],
+                                    strides=[1, 4, 4, 1],
                                     padding='SAME')
 
-    # input size of the image : 400*400*24
+    # input size of the image : 200*200*24
     conv2d_layer_two = tf.contrib.layers.convolution2d(
         pool_layer_one,
-        num_outputs=24,  # More output channels means an increase in the number of filters
+        num_outputs=32,  # More output channels means an increase in the number of filters
         kernel_size=(5, 5),
         activation_fn=tf.nn.relu,
         stride=(1, 1))
     pool_layer_two = tf.nn.max_pool(conv2d_layer_two,
-                                    ksize=[1, 3, 3, 1],
-                                    strides=[1, 3, 3, 1],
+                                    ksize=[1, 4, 4, 1],
+                                    strides=[1, 4, 4, 1],
                                     padding='SAME')
 
-    # input size of the image : 200*200*48
+    # input size of the image : 50*50*48
     conv2d_layer_three = tf.contrib.layers.convolution2d(
         pool_layer_two,
-        num_outputs=36,  # More output channels means an increase in the number of filters
+        num_outputs=64,  # More output channels means an increase in the number of filters
         kernel_size=(5, 5),
         activation_fn=tf.nn.relu,
         stride=(1, 1))
     pool_layer_three = tf.nn.max_pool(conv2d_layer_three,
+                                    ksize=[1, 5, 5, 1],
+                                    strides=[1, 5, 5, 1],
+                                    padding='SAME')
+
+    # input size of the image : 10*10*48
+    conv2d_layer_four = tf.contrib.layers.convolution2d(
+        pool_layer_three,
+        num_outputs=128,  # More output channels means an increase in the number of filters
+        kernel_size=(5, 5),
+        activation_fn=tf.nn.relu,
+        stride=(1, 1))
+    pool_layer_four = tf.nn.max_pool(conv2d_layer_four,
+                                    ksize=[1, 5, 5, 1],
+                                    strides=[1, 5, 5, 1],
+                                    padding='SAME')
+
+    # input size of the image : 2*2*48
+    conv2d_layer_five = tf.contrib.layers.convolution2d(
+        pool_layer_four,
+        num_outputs=256,  # More output channels means an increase in the number of filters
+        kernel_size=(5, 5),
+        activation_fn=tf.nn.relu,
+        stride=(1, 1))
+    pool_layer_five = tf.nn.max_pool(conv2d_layer_five,
                                     ksize=[1, 2, 2, 1],
                                     strides=[1, 2, 2, 1],
                                     padding='SAME')
 
-    # input size of the image : 100*100*64
+    # input size of the image : 1*1*256
     print('here')
     flattened_layer_two = tf.reshape(
-        pool_layer_three,
+        pool_layer_five,
         [
             batch_size,  # Each image in the image_batch
             -1  # Every other dimension of the input
@@ -112,8 +136,11 @@ def convolution_model(float_image_batch):
 # load and display the image according to the given path
 def image_load_display_preprocessing(path="./Her_Face/IMG_4302.jpg"):
 
+    #plt.ion()
     img_empty = Image.new('RGBA', (800, 800), color='white')
     img1 = Image.open(path)
+    '''
+    # temporally close the image pre-process
     # generate a random number to rotate the image
     rotate_num = np.random.randint(0, 360, dtype='int32')
     #print(rotate_num)
@@ -124,6 +151,7 @@ def image_load_display_preprocessing(path="./Her_Face/IMG_4302.jpg"):
     #print(blur_num)
     img1 = img1.filter(ImageFilter.GaussianBlur(radius=blur_num))
     #
+    '''
     width, height = img1.size
     width_mul = 800. / width
     height_mul = 800. / height
@@ -141,10 +169,12 @@ def image_load_display_preprocessing(path="./Her_Face/IMG_4302.jpg"):
 
     img_empty = img_empty.convert('L')
 
-    plt.imshow(img_empty)
-    plt.show()
+    #plt.imshow(img_empty)
+    #plt.show()
+
     # add sleep time before each time of training to show the process
-    #time.sleep(0.1)
+    #time.sleep(0.2)
+    #plt.close()
     img_empty = np.asarray(img_empty)
     return img_empty
 
@@ -162,15 +192,15 @@ def generate_test_data(test_label, test_path):
         test = image_load_display_preprocessing(image_temp)
         label = test_temp
         if label == 'My_Face':
-            one_hot_label = [1, 0, 0, 0, 0]
+            one_hot_label = [1., 0., 0., 0., 0.]
         elif label == 'Her_Face':
-            one_hot_label = [0, 1, 0, 0, 0]
+            one_hot_label = [0., 1., 0., 0., 0.]
         elif label == 'Don_Face':
-            one_hot_label = [0, 0, 1, 0, 0]
+            one_hot_label = [0., 0., 1., 0., 0.]
         elif label == 'Mom_Face':
-            one_hot_label = [0, 0, 0, 1, 0]
+            one_hot_label = [0., 0., 0., 1., 0.]
         else:
-            one_hot_label = [0, 0, 0, 0, 1]
+            one_hot_label = [0., 0., 0., 0., 1.]
 
         image_sequence.append(test)
         label_sequence.append(one_hot_label)
@@ -180,30 +210,35 @@ def generate_test_data(test_label, test_path):
     return np_image_sequence, np_label_sequence
 
 
+def softmax(x):
+    return np.exp(x)/np.sum(np.exp(x),axis=0)
+
+
 # pick num = batch_size images and load for training
 def generate_data(image_label, image_path):
     # dataset has 143 images, batch_size = 10
-    random_begining = np.random.randint(0, 121)
+
     image_sequence = []
     label_sequence = []
     print(image_path)
 
     for i in range(10):
-        image_temp = image_path[random_begining+i]
-        label_temp = image_label[random_begining+i]
+        random_begining = np.random.randint(0, 131)
+        image_temp = image_path[random_begining]
+        label_temp = image_label[random_begining]
 
         image = image_load_display_preprocessing(image_temp)
         label = label_temp
         if label == 'My_Face':
-            one_hot_label = [1, 0, 0, 0, 0]
+            one_hot_label = [1.0, 0., 0., 0., 0.]
         elif label == 'Her_Face':
-            one_hot_label = [0, 1, 0, 0, 0]
+            one_hot_label = [0., 1., 0., 0., 0.]
         elif label == 'Don_Face':
-            one_hot_label = [0, 0, 1, 0, 0]
+            one_hot_label = [0., 0., 1., 0., 0.]
         elif label == 'Mom_Face':
-            one_hot_label = [0, 0, 0, 1, 0]
+            one_hot_label = [0., 0., 0., 1., 0.]
         else:
-            one_hot_label = [0, 0, 0, 0, 1]
+            one_hot_label = [0., 0., 0., 0., 1.]
 
         image_sequence.append(image)
         label_sequence.append(one_hot_label)
@@ -223,8 +258,8 @@ X = tf.placeholder(tf.float32, [batch_size, 800, 800, 1])
 Y = tf.placeholder(tf.float32, [batch_size, 5])
 # tensor flow
 predict = convolution_model(X)
-cost_func = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=predict, labels=Y))
-optimizer = tf.train.AdamOptimizer(1e-2).minimize(cost_func)
+cost_func = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits=predict, labels=Y))
+optimizer = tf.train.AdamOptimizer(1e-4).minimize(cost_func)
 
 saver = tf.train.Saver(max_to_keep=4)
 with tf.Session() as sess:
@@ -252,9 +287,24 @@ with tf.Session() as sess:
         test_image_sequence = test_image_sequence.reshape([-1, 800, 800, 1])
         _, test_cost = sess.run([optimizer, cost_func], feed_dict={X: test_image_sequence, Y: test_label_sequence})
         print('cost function', test_cost)
+        print('Label_real')
+        print(test_label_sequence)
+        print('Label_cal')
+        predict_test = sess.run([predict], feed_dict={X: test_image_sequence})
+        temp_list = []
+        predict_test = np.asarray(predict_test)
+        for test_num in range(10):
+            a = predict_test[0][test_num]
+            aa = softmax(a)
+            temp_list.append(aa)
+
+        print(np.asarray(temp_list))
         print(' ')
         time_end = time.time()
         print('time cost', time_end - time_start)
+        print(' ')
+        print(' ')
+        print(' ')
 
 sess.close()
 del sess
